345879cb04f7775a6a1154a5df698a4b037f4974
Sepherd <78238871+Sepherd@users.noreply.github.com>
2021-11-19 16:12:27 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -174,3 +174,29 @@ function conversione(input, output) {
         $("#valori").append("<p>" + input + " " + temperatura[1][toConvert] + " = " + output + " " + temperatura[1][converted] + "</p>");
     };
 };
+
+/****MOUSE/TOUCH****/
+
+document.addEventListener("touchstart", touch2Mouse, true);
+document.addEventListener("touchmove", touch2Mouse, true);
+document.addEventListener("touchend", touch2Mouse, true);
+
+function touch2Mouse(e)
+{
+  var theTouch = e.changedTouches[0];
+  var mouseEv;
+
+  switch(e.type)
+  {
+    case "touchstart": mouseEv="mousedown"; break;  
+    case "touchend":   mouseEv="mouseup"; break;
+    case "touchmove":  mouseEv="mousemove"; break;
+    default: return;
+  }
+
+  var mouseEvent = document.createEvent("MouseEvent");
+  mouseEvent.initMouseEvent(mouseEv, true, true, window, 1, theTouch.screenX, theTouch.screenY, theTouch.clientX, theTouch.clientY, false, false, false, false, 0, null);
+  theTouch.target.dispatchEvent(mouseEvent);
+
+  e.preventDefault();
+};
